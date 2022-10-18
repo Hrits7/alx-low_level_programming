47b9f32e6407a5e8c8d0a10817f196b3903d6de6
Hrits7 <hirthikbudhoo@gmail.com>
2022-10-18 11:04:01 -0700

    _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * main - prints the string
+ * Return: 0
+ */
+
+int main(void)
+{
+	char c[] = "_putchar";
+	int i;
+
+	for (i = 0; i < 9; i++)
+	{
+		_putchar(c[i]);
+	}
+	_putchar('\n');
+	return (0);
+}
